2ebbe80c0584093f7aaa60100dadfebf51727dcc
Herikle, Filho de Odin <herikle.mesquita@escolar.ifrn.edu.br>
2019-04-08 15:42:37 -0300

    alteranodo rotas para página inicial, criando outro controller para alternar entre as páginas

--- app/controllers/IndexController.java
+++ app/controllers/IndexController.java
@@ -4,12 +4,12 @@ import play.mvc.*;
 import javax.inject.Inject;
 /**
  * This controller contains an action to handle HTTP requests
- * to the application's home page.
+ * to the application's index page.
  */
-public class HomeController extends Controller {
+public class IndexController extends Controller {
 
     public Result index() {
-        return ok(views.html.pages.home.render());
+        return ok(views.html.index.render());
     }
 
 }
