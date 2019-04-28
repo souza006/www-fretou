99a0d3e5368a4c7466e3253cea1918c0d4f8f54c
David Héricles de Lima Silva <david.h@academico.ifrn.edu.br>
2019-04-28 12:40:08 -0300

    Funcao para testar a rota para ver pagina de anuncio.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -12,4 +12,8 @@ public class HomeController extends Controller {
         return ok(views.html.pages.home.render());
     }
 
+    public Result anuncio(int id){
+        return ok(views.html.pages.anuncio.render(id));
+    }
+
 }
