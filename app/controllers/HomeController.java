c8749b4949f836ee4dc32c37680a129e939fa148
David Héricles de Lima Silva <david.h@academico.ifrn.edu.br>
2019-04-29 08:02:01 -0300

    Corrigida as rotas para exibir um anuncio corretamente.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -11,9 +11,4 @@ public class HomeController extends Controller {
     public Result index() {
         return ok(views.html.pages.home.render());
     }
-
-    public Result anuncio(int id){
-        return ok(views.html.pages.anuncio.render(id));
-    }
-
 }
