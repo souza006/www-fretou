5a7a5801e286b2767e9b7c46852b179f317e2de1
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-03 19:58:38 -0300

    adicionando usuários estáticos ao sistema e métodos de retorno de contratantes e fretistas

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -1,12 +1,57 @@
+package models;
+
+import java.util.*;
+
 public class Usuario {
+
+  public static String TIPO_CONTRATANTE = "contratante";
+  public static String TIPO_FRETISTA = "fretista";
 
   private int id;
   private String tipo;
+  private String nome;
+
+  private static Set<Usuario> usuarios;
 
 
-  public Usuario(int id, String tipo){
+
+  public Usuario(int id, String tipo, String nome){
     this.id = id;
     this.tipo = tipo;
+    this.nome = nome;
+  }
+
+  static {
+    usuarios = new HashSet<>();
+
+    usuarios.add(new Usuario(1, TIPO_CONTRATANTE, "Daniel Souza"));
+    usuarios.add(new Usuario(11, TIPO_FRETISTA, "David Héricles"));
+    usuarios.add(new Usuario(111, TIPO_CONTRATANTE, "Eduardo Riev"));
+    usuarios.add(new Usuario(1111, TIPO_FRETISTA, "Elvis Silva"));
+    usuarios.add(new Usuario(11111, TIPO_CONTRATANTE, "Daniel Souza"));
+    usuarios.add(new Usuario(111111, TIPO_FRETISTA, "Hérikle Mesquita"));
+  }
+
+  public static Set<Usuario> all() {
+    return usuarios;
+  }
+
+  public static Set<Usuario> fretistas() {
+    Set<Usuario> fretistas = new HashSet<>();
+    for (Usuario usuario : usuarios)
+      if (usuario.tipo.equals(TIPO_FRETISTA))
+        fretistas.add(usuario);
+
+    return fretistas;
+  }
+
+  public static Set<Usuario> contratantes() {
+    Set<Usuario> contratantes = new HashSet<>();
+    for (Usuario usuario : usuarios)
+      if (usuario.tipo.equals(TIPO_CONTRATANTE))
+        contratantes.add(usuario);
+
+    return contratantes;
   }
 
   public void setId(int id){
